ca460556abd9cebdde33b985243687153b535a1b
gerardogama <153300914+gerardogama@users.noreply.github.com>
2024-04-15 23:38:32 +0000

    ejercicio

--- src/app.js
+++ src/app.js
@@ -6,6 +6,24 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+
+  function generadorDominios(pronoun, adj, noun) {
+    let combinaciones = [];
+    for (let pronombre of pronoun) {
+      for (let adjetivo of adj) {
+        for (let sustantivo of noun) {
+          let dominio = `${pronombre}${adjetivo}${sustantivo}.com`;
+          combinaciones.push(dominio);
+        }
+      }
+    }
+    return combinaciones;
+  }
+  //llamo a la funcion
+  const dominios = generadorDominios(pronoun, adj, noun);
+
+  console.log(dominios);
 };
